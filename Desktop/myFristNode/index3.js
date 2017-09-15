3331a810a229ac61a4606da7e6b015a5a9975ab8
nuttaphat santiphan <nuttaphat.santiphan@g.swu.ac.th>
2017-09-15 23:28:35 +0700

    add file

---
+++ Desktop/myFristNode/index3.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.set('num', 1);
+var num = app.get('num');
+console.log(num); //1
+
+
+app.use(function (req, res, next) {
+    console.log('Record timestamp');
+    next();
+});
+
+app.use(bodyParser.json());
+
+app.post('/calculator/rest/minus', function (req, res, next) {
+    var body = req.body;
+    var x = body.x;
+    var result = 0;
+    var object ={
+        "x":x,
+
+        "result":(x*1024)/1099511627776
+    }
+    res.json(object);
+});
+
+app.get('/help', function (req, res, next) {
+    res.send('Help me');
+});
+
+app.listen(3000, function() {
+    console.log('Starting node on port 3000')
+});
